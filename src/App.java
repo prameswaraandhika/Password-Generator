e9cdff112283c3a1eb276badd08a9b77bae48811
Andhikap <prameswaara@gmail.com>
2023-06-22 10:06:10 +0700

    Refactored code: Improved readability, removed redundant code, and adhered to clean code principles.

--- src/App.java
+++ src/App.java
@@ -2,27 +2,37 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class App {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=<>?";
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String doGenerated;
+
         do {
-            System.out.print("Input length? ");
-            byte inputLength = sc.nextByte();
-            generatePassword(inputLength);
-            sc.nextLine();
-            System.out.print("Do generate again?[y/n]");
+            System.out.print("Input password length: ");
+            int inputLength = sc.nextInt();
+            sc.nextLine(); // Consume the newline character its mean 'enter'
+
+            String password = generatePassword(inputLength);
+            System.out.println("Generated Password: " + password);
+
+            System.out.print("Generate again? [y/n]: ");
             doGenerated = sc.nextLine();
         } while (doGenerated.equalsIgnoreCase("y"));
+
+        sc.close();
     }
 
-    private static void generatePassword(byte inputLength) {
-        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=<>?";
-        StringBuilder pass = new StringBuilder();
-        for (int i = 0; i < inputLength; i++) {
-            int index = new Random().nextInt(chars.length());
-            pass.append(chars.charAt(index));
+    private static String generatePassword(int length) {
+        StringBuilder passwordBuilder = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            char randomChar = CHARACTERS.charAt(randomIndex);
+            passwordBuilder.append(randomChar);
         }
-        System.out.printf("Password: %s \n", pass);
+
+        return passwordBuilder.toString();
     }
 }
